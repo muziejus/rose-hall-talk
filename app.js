b121093b5090dc5a34afb1ee6e63d55b144e68cb
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-11-09 19:03:14 -0600

    lint app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-$("p").filter( function() {
-  return (this.textContent || this.innerText).match(/^-->/);
-}) .addClass("credit").html(function(i, html){
-  return html.replace(/^--&gt; /, "");
-});
+$("#bibliography")
+  .nextAll("ul")
+  .addClass("bibliography");
 
-$("li").filter( function() {
-  return (this.textContent || this.innerText).match(/^==/);
-}) .addClass("fragment").html(function(i, html){
-  return html.replace(/^== /, "");
-});
+$("p")
+  .filter(function() {
+    return (this.textContent || this.innerText).match(/^-->/);
+  })
+  .addClass("credit")
+  .html(function(i, html) {
+    return html.replace(/^--&gt; /, "");
+  });
 
+$("li")
+  .filter(function() {
+    return (this.textContent || this.innerText).match(/^==/);
+  })
+  .addClass("fragment")
+  .html(function(i, html) {
+    return html.replace(/^== /, "");
+  });
